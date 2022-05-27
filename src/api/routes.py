723124f211803599a8b12a89b8997b8ce56b90e7
avokeys <alexander.ayala.palacin@gmail.com>
2022-05-27 22:22:43 +0000

    successful login user endpoint

--- src/api/routes.py
+++ src/api/routes.py
@@ -8,11 +8,27 @@ from api.utils import generate_sitemap, APIException
 api = Blueprint('api', __name__)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+@api.route('/signup', methods=['POST'])
+def signup_user():
+    body = request
+    email = request.json.get('email')
+    password = request.json.get('password')
+    if body is None:
+        return "body is empty", 400
+    if not email:
+        return "email is empty", 400
+    if not password:
+        return "password is empty", 400
+    
+    check_user = User.query.filter_by(email=email).first()
+    if check_user is not None:
+        return "user already exists", 409
+    user = User(email= email, password=password, is_active=True)
+    db.session.add(user)
+    db.session.commit()
 
     response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
+        "message": "Your account has been registered successfully", "user": user.serialize()
     }
 
-    return jsonify(response_body), 200
+    return jsonify(response_body ), 200
