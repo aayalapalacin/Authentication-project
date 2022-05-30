50b7b222a72e98c8b51504748cef95e2f144f49e
avokeys <alexander.ayala.palacin@gmail.com>
2022-05-30 20:54:17 +0000

    deleted navbar and footer

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -11,16 +11,10 @@ export const Home = () => {
       <h1>Sign Up/ Log in</h1>
 
       <Register />
-      <div className="alert alert-info">
+      {/* <div className="alert alert-info">
         {store.message ||
           "Loading message from the backend (make sure your python backend is running)..."}
-      </div>
-      <p>
-        This boilerplate comes with lots of documentation:{" "}
-        <a href="https://github.com/4GeeksAcademy/react-flask-hello/tree/95e0540bd1422249c3004f149825285118594325/docs">
-          Read documentation
-        </a>
-      </p>
+      </div> */}
     </div>
   );
 };
